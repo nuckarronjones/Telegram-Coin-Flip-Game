1f565aa7d48889671b05e383879df03c0a0a9b5f
Nicholas Jones <njonesgrad@gmail.com>
2025-01-10 23:55:16 +0100

    Frontend Components
    Finish more or less the front end. Still need to address that the app looks different for different mobile phones in regards to screen height, and apply stylings for desktop soon.

---
+++ frontend/src/app/game-board/bet-options/bet-options.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bet-options',
+  imports: [],
+  templateUrl: './bet-options.component.html',
+  styleUrl: './bet-options.component.scss'
+})
+export class BetOptionsComponent {
+
+}
--- frontend/src/app/game-board/game-board.component.ts
+++ frontend/src/app/game-board/game-board.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { UserSelectionComponent } from './user-selection/user-selection.component';
+import { BetOptionsComponent } from './bet-options/bet-options.component';
 
 @Component({
   selector: 'app-game-board',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserSelectionComponent, BetOptionsComponent],
   standalone: true,
   templateUrl: './game-board.component.html',
   styleUrl: './game-board.component.scss'
